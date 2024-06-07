f880d854a882d5d2d0ffe5640c76c11def0ec9bd
Edmundo Corona <113280885+edmundoCA@users.noreply.github.com>
2024-06-07 02:30:00 +0000

    Added console output

---
+++ main.py
@@ -0,0 +1,52 @@
+RUBIK_COLORS = ("yellow", "white", "orange", "red", "blue", "green")
+RUBIK_FACES = ("up", "down", "left", "right", "front", "back")
+rubik_cube = {}
+
+def main():
+    set_initial_state()
+    display_in_console_pretty()
+
+def display_in_console():
+    for key in rubik_cube.keys():
+        print("_________")
+        print(key)
+        print("---------")
+        for row in rubik_cube[key]:
+            print(row)
+
+def display_in_console_pretty():
+    face_console_output = " " * 8 + "-" * 9 + "\n"
+    face_console_output += generate_face_console_output("up")
+    face_console_output += "-" * 33
+
+    for row in range(3):
+        row_console_output = "\n"
+        for face in ["left", "front", "right", "back"]:
+            row_console_output += "| "
+            for col in rubik_cube[face][row]:
+                row_console_output += col[0] + " "
+        row_console_output += "|"
+        face_console_output += row_console_output
+    print(face_console_output)
+
+    face_console_output = "-" * 33 + "\n"
+    face_console_output += generate_face_console_output("down")
+    face_console_output += " " * 8 + "-" * 9
+    print(face_console_output)
+
+def generate_face_console_output(face):
+    face_console_output = ""
+    for row in rubik_cube[face]:
+        row_console_output = " " * 8 + "| "
+        for col in row:
+            row_console_output += col[0] + " "
+        row_console_output += "|"
+        face_console_output += row_console_output + "\n"
+    return face_console_output
+            
+def set_initial_state():
+    for face, color in zip(RUBIK_FACES, RUBIK_COLORS):
+        rubik_cube[face] = [[color for _ in range (3)] for _ in range (3)]
+
+if __name__ == '__main__':
+    main()
